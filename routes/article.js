75d5d875a6a5129f570dcd4bc8525fd678c80e83
rodgersj097 <rodgersj097@gmail.com>
2019-07-20 21:55:37 -0400

    added promis to requests to make sure articles loads on first try

--- routes/article.js
+++ routes/article.js
@@ -2,20 +2,22 @@ var express = require("express");
 var router = express.Router();
 var Request = require("request");
 var articles = [];
+var rp = require("request-promise");
 //= require('../config/articleRequest')
 
 console.log("Starting to fetch articles");
 
 /* GET users listing. */
-router.get("/:route", function(req, res, next) {
-  Request.get("http://72.141.30.148/articles", (err, _res, body) => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("articles Fetched Succesfully");
-
-    articles = JSON.parse(body);
-  });
+router.get("/:route", async function(req, res, next) {
+  await rp({ url: "http://72.141.30.148/articles", json: true })
+    .then(function(body) {
+      console.log("articles Fetched Succesfully");
+      articles = body;
+      console.log(articles);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
   const article = articles.find(art => art.url === req.params.route);
   console.log(req.params.route);
--- routes/collections.js
+++ routes/collections.js
@@ -1,20 +1,22 @@
 var express = require("express");
 var router = express.Router();
 var Request = require("request");
+var rp = require("request-promise");
 //var articles = require('../config/articleRequest')
 var articles = [];
 
 console.log(articles);
 /* GET users listing. */
 router.get("/", async function(req, res, next) {
-  Request.get("http://72.141.30.148/articles", (err, _res, body) => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("articles Fetched Succesfully");
+  await rp({ url: "http://72.141.30.148/articles", json: true })
+    .then(function(body) {
+      console.log("articles Fetched Succesfully");
+      articles = body;
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
-    articles = JSON.parse(body);
-  });
   res.render("collections", { title: "MHCM", articles });
 });
 
